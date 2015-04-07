d3e944633db84125a4ff4cd78bf882dff8460488
Alexander Koval <kovalidis@gmail.com>
2015-04-07 19:40:15 +0300

    Ede project for emacs

---
+++ .ede-project.el
@@ -0,0 +1,9 @@
+;; (ede-java-root-project "GroupScoreQuery"
+;;                        :file "pom.xml"
+;;                        :srcroot '("src"))
+
+(global-ede-mode t)
+
+(semantic-mode 1)
+
+(semantic-add-system-include "../lucene-solr/lucene/core/src/java/")
